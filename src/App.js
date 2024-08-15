228033588d0ba6208a0e5430bdc004c1201083d0
zain <zainazhar303@gmail.com>
2024-08-15 12:11:18 +0500

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import PersonIcon from '@mui/icons-material/Person';
+// import PersonIcon from '@mui/icons-material/Person';
 import Home from './pages/home/Home';
-import Profile from './components/profile/Profile';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register'
+// import Profile from './components/profile/Profile';
+// import Login from './pages/login/Login';
+// import Register from './pages/register/Register'
 function App() {
   return (
     // <Register />
